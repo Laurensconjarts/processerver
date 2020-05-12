52db6fed6efd5240e891bb1a76afd57feaae64ec
Laurensconjardo <laurens@conjarts.eu>
2020-05-12 14:12:20 +0200

    insert signaal

--- apitest.py
+++ apitest.py
@@ -4,6 +4,7 @@ def api_con():
     global response
     response = requests.get("http://145.220.75.101:8005/api/v1/locations/sutoka")
     data = response.json()
+    for each in data['locations']:
+        print(each['d'])
 
-    print(data)
 api_con()
