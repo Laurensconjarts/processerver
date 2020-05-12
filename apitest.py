138145793cb0d80ebf290252fecee0d54aeb30f3
Laurensconjardo <laurens@conjarts.eu>
2020-05-12 13:40:57 +0200

    insert signaal

--- apitest.py
+++ apitest.py
@@ -2,7 +2,7 @@
 import requests
 def api_con():
     global response
-    response = requests.get("http://145.220.75.101:8005/api/v1/locations/sutoka")
+    response = requests.get("1http://145.220.75.101:8005/api/v1/locations/sutoka")
 
 
     print(response.text)
