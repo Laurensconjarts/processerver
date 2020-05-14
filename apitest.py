2a12ec3d02eee7f69919a07eaf0a7cbfe9abd8ad
Laurensconjardo <laurens@conjarts.eu>
2020-05-14 11:17:59 +0200

    insert signaal

--- apitest.py
+++ apitest.py
@@ -1,10 +1,12 @@
 
 import requests
+testlist = []
 def api_con():
     global response
     response = requests.get("http://145.220.75.101:8005/api/v1/locations/sutoka")
     data = response.json()
     for each in data['locations']:
-        print(each['sensors']['d'])
-
+        testlist.append(each['sensors']['d'])
+        testlist.append(each['sensors']['s']['wifi'])
+    print(testlist)
 api_con()
