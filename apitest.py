d9c2f5135dd439654c7a8852fa651086d88a1421
Laurensconjardo <laurens@conjarts.eu>
2020-05-14 17:46:35 +0200

    insert signaal

--- apitest.py
+++ apitest.py
@@ -1,22 +1,50 @@
 
 import requests
-testlist = []
-testlist2 = []
-testlist3 = []
+devices = []
+macdata = []
+macadres = []
+locatie = []
+sterkte = []
 def api_con():
     global response
     response = requests.get("http://145.220.75.101:8005/api/v1/locations/sutoka")
     data = response.json()
     for each in data['locations']:
-        testlist.append(each['sensors']['d'])
-    print(testlist)
+        devices.append(each['sensors']['d'])
+
+
     count = 0
-    while count < 5:
+    c = len(devices)
+    while count < c:
 
         data2 = data['locations'][count]['sensors']['s']['wifi']
         for each in data2:
-            testlist2.append(each)
-        data3 = testlist2[count]
-        print(data3)
+            macdata.append(each)
+        macadres.append(macdata[0])
+        macdata.clear()
         count += 1
+    count3 = 0
+    while count3 < c:
+        locatie.append(data['locations'][count3]['prediction']['location'])
+        count3 += 1
+    count4 = 0
+    while count4 < c:
+        sterkte.append(data['locations'][count4]['prediction']['probability'])
+        count4 += 1
+    count1 = 0
+    while count1 < c:
+        mac = macadres[0]
+
+        del macadres[0]
+        device = devices[0]
+        del devices[0]
+        node = locatie[0]
+        del locatie[0]
+        signaal = sterkte[0]
+        del sterkte[0]
+        apparaat_insert(mac, device)
+        def Signaal_insert(signaal, node)
+        count1 += 1
+
+
 api_con()
--- processserver.py
+++ processserver.py
@@ -1,6 +1,10 @@
 import pymssql
 import requests
-
+devices = []
+macdata = []
+macadres = []
+locatie = []
+sterkte = []
 
 def db_con():
     global conn
@@ -16,12 +20,59 @@ def api_con():
 db_con()
 api_con()
 
-mac = input("mac")
-appa = input("appa")
-signaal = input("signaal")
-node = input("node")
+mac = 0
+device = 0
+signaal = 0
+node = 0
 
-def apparaat_insert():
+def api_con2():
+    global response
+    response = requests.get("http://145.220.75.101:8005/api/v1/locations/sutoka")
+    data = response.json()
+    for each in data['locations']:
+        devices.append(each['sensors']['d'])
+
+
+    count = 0
+    c = len(devices)
+    while count < c:
+
+        data2 = data['locations'][count]['sensors']['s']['wifi']
+        for each in data2:
+            macdata.append(each)
+        macadres.append(macdata[0])
+        macdata.clear()
+        count += 1
+    count3 = 0
+    while count3 < c:
+        locatie.append(data['locations'][count3]['prediction']['location'])
+        count3 += 1
+    count4 = 0
+    while count4 < c:
+        sterkte.append(data['locations'][count4]['prediction']['probability'])
+        count4 += 1
+    count1 = 0
+    while count1 < c:
+        mac = macadres[0]
+
+        del macadres[0]
+        device = devices[0]
+        del devices[0]
+        node = locatie[0]
+        print(node)
+        del locatie[0]
+        signaal = sterkte[0]
+        print(signaal)
+        del sterkte[0]
+        apparaat_insert(mac, device)
+        Signaal_insert(signaal, node)
+        count1 += 1
+
+
+
+
+
+def apparaat_insert(mac, device):
     global idn
     try:
         querie = "select max(ApparaatID) from Apparaat"
@@ -29,7 +80,7 @@ def apparaat_insert():
         result = cursor.fetchone()
         ido = result[0]
         idn = ido + 1
-        querie1 = f"INSERT INTO Apparaat(ApparaatID, MACADRES, Typeapparaat) VALUES ({idn}, '{mac}', '{appa}');"
+        querie1 = f"INSERT INTO Apparaat(ApparaatID, MACADRES, Typeapparaat) VALUES ({idn}, '{mac}', '{device}');"
         cursor.execute(querie1)
         conn.commit()
     except pymssql.Error as e:
@@ -39,11 +90,10 @@ def apparaat_insert():
         result = cursor.fetchone()
         idn = result[0]
         print(idn)
-def Signaal_insert():
-        querie4 = f"INSERT INTO Signaal(Datum, ApparaatID, Signaalsterkte,Node) VALUES (GETDATE(), {idn}, {signaal}, {node});"
+def Signaal_insert(signaal, node):
+        querie4 = f"INSERT INTO Signaal(ApparaatID, Signaalsterkte,Node) VALUES ({idn}, {signaal}, '{node}');"
         cursor.execute(querie4)
 
 
 
-apparaat_insert()
-Signaal_insert()
+api_con2()
